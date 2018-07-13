ab7e833cae51dd02dfb2afc51155d540ee0d263d
j1angvei <mjiangwei@outlook.com>
2018-07-13 18:45:19 +0800

    add mini mode file

---
+++ src/main/java/cn/j1angvei/adbfx/mini/MiniModeController.java
@@ -0,0 +1,20 @@
+package cn.j1angvei.adbfx.mini;
+
+import cn.j1angvei.adbfx.BaseController;
+
+public class MiniModeController extends BaseController {
+    @Override
+    protected void initArguments() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
